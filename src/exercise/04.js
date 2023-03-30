204d12f892c97b21f109098b596905e46372a50c
Thomas Newman <thomblweed@gmail.com>
2023-03-30 21:00:40 +0100

    exercise 4 updates with local storage

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -3,67 +3,76 @@
 
 import * as React from 'react'
 
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = React.useState(() => {
+    let currentValue
+
+    try {
+      currentValue = JSON.parse(
+        localStorage.getItem(key) || String(initialValue),
+      )
+    } catch (error) {
+      currentValue = initialValue
+    }
+
+    return currentValue
+  })
+
+  React.useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [value, key])
+
+  return [value, setValue]
+}
+
+const Square = ({value, onSquareClick}) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  )
+}
+
+const emptyBoard = Array(9).fill(null)
+
 function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  const [squares, setSquares] = useLocalStorage('squares', emptyBoard)
+  // const [squares, setSquares] = React.useState(() => storedSquares)
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
 
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
-
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
+    if (winner || squares[square]) {
+      return
+    }
+
+    const squaresCopy = [...squares]
+    squaresCopy[square] = nextValue
+    setSquares(squaresCopy)
   }
 
   function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
-  }
-
-  function renderSquare(i) {
-    return (
-      <button className="square" onClick={() => selectSquare(i)}>
-        {squares[i]}
-      </button>
-    )
+    setSquares(emptyBoard)
   }
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
+        <Square value={squares[0]} onSquareClick={() => selectSquare(0)} />
+        <Square value={squares[1]} onSquareClick={() => selectSquare(1)} />
+        <Square value={squares[2]} onSquareClick={() => selectSquare(2)} />
       </div>
       <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
+        <Square value={squares[3]} onSquareClick={() => selectSquare(3)} />
+        <Square value={squares[4]} onSquareClick={() => selectSquare(4)} />
+        <Square value={squares[5]} onSquareClick={() => selectSquare(5)} />
       </div>
       <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
+        <Square value={squares[6]} onSquareClick={() => selectSquare(6)} />
+        <Square value={squares[7]} onSquareClick={() => selectSquare(7)} />
+        <Square value={squares[8]} onSquareClick={() => selectSquare(8)} />
       </div>
       <button className="restart" onClick={restart}>
         restart
